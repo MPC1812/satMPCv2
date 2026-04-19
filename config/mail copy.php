380a510aea8c0ae6b9d77da8b2be4e6a6b67be37
Blackmoon1812 <93385348+Blackmoon1812@users.noreply.github.com>
2026-04-19 17:38:39 +0200

    Reparando gmail para envio de correos en Railway.app

--- config/mail copy.php
+++ config/mail copy.php
@@ -36,23 +36,19 @@ return [
     */
 
     'mailers' => [
+
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'scheme' => env('MAIL_SCHEME'),
+            'url' => env('MAIL_URL'),
+            'host' => env('MAIL_HOST', '127.0.0.1'),
+            'port' => env('MAIL_PORT', 2525),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'timeout' => 30,
-            // Añade este bloque de 'stream' al final del array smtp:
-            'stream' => [
-                'ssl' => [
-                    'allow_self_signed' => true,
-                    'verify_peer' => false,
-                    'verify_peer_name' => false,
-                ],
-            ],
+            'timeout' => null,
+            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
+
         'ses' => [
             'transport' => 'ses',
         ],
